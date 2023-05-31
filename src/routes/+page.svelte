f89886550f5fcbd1837fb1989793d80285bce92e
Samuel Middendorp <samiezaurus@gmail.com>
2023-05-31 20:32:14 +0200

    Add typeface logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>AeroPod</h1>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Kodchasan:wght@200&family=Urbanist:wght@100&display=swap');
+    :global(*) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    h1 {
+        font-family: "Kodchasan", sans-serif;
+        font-size: 3rem;
+        padding: 1rem;
+        font-weight: 200;
+    }
+</style>
