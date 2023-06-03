be14d92b33db2f5b0ffabaab5020cb47112e5d4f
Samuel Middendorp <samiezaurus@gmail.com>
2023-06-03 15:13:21 +0200

    Replace explicit anchor with implicit id

--- src/routes/wisdom/+page.svelte
+++ src/routes/wisdom/+page.svelte
@@ -8,7 +8,7 @@
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
 </p>
-<NavigateableHeading title={"Some more wisdom"} anchor={"more-wisdom"}/>
+<NavigateableHeading title={"Some more wisdom"}/>
 <p>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
@@ -28,7 +28,27 @@
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
 </p>
-<NavigateableHeading title={"Some more more wisdom"} anchor={"more-more-wisdom"}/>
+<NavigateableHeading title={"Some more more wisdom"}/>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+</p>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+</p>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
+</p>
+<NavigateableHeading title={"Some more more more wisdom"}/>
 <p>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed eligendi iusto cumque minus, blanditiis odio ipsum dignissimos laudantium totam, ad omnis reiciendis incidunt et tempore eaque sunt similique suscipit nobis!
