153c97d7c908bb6747b5ba982c799b9b2cc17143
Samuel Middendorp <samiezaurus@gmail.com>
2023-06-11 18:46:32 +0200

    Implement fake api store to test

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -1,79 +1,40 @@
 <script lang="ts">
     import Stat from "$lib/components/Stat.svelte";
-import type { Recipe } from "$lib/model/model";
+    import { getRecipes } from "$lib/stores/FakeAPIStore";
 
-    let recipes: Recipe[] = [
-        {
-            name: "Recipe 1",
-            description: "lorem ipsum coffe blablba recipe, nice to drink blabla",
-            intensity: 7,
-            preperationTime: 360,
-            steps: [],
-        },
-        {
-            name: "Recipe 2",
-            description: "lorem ipsum coffe blablba recipe, nice to drink blabla",
-            intensity: 8,
-            preperationTime: 360,
-            steps: [],
-        },
-        {
-            name: "Recipe 3",
-            description: "lorem ipsum coffe blablba recipe, nice to drink blabla",
-            intensity: 10,
-            preperationTime: 30,
-            steps: [],
-        },
-        {
-            name: "Recipe 4",
-            description: "lorem ipsum coffe blablba recipe, nice to drink blabla",
-            intensity: 2,
-            preperationTime: 70,
-            steps: [],
-        },
-        {
-            name: "Recipe 5",
-            description: "lorem ipsum coffe blablba recipe, nice to drink blabla",
-            intensity: 1,
-            preperationTime: 60,
-            steps: [],
-        },
-        {
-            name: "Recipe 6",
-            description: "lorem ipsum coffe blablba recipe, nice to drink blabla",
-            intensity: 7,
-            preperationTime: 45,
-            steps: [],
-        },
-    ];
+    let recipes = getRecipes();
 </script>
 
 <div class="recipes">
     {#each recipes as recipe}
-    <div class="recipe aero-panel">
-        <button on:click={() => recipes = recipes.filter((x) => x != recipe)}>-</button>
-        <a draggable="false" href="recipes/22">
-        <h5>{recipe.name}</h5>
-        <h4>{recipe.description}</h4>
-        <p>Intensity: {recipe.intensity}</p>
-        <Stat percentage={(recipe.intensity / 10 * 100)}/>
-        <p>Prep time: {recipe.preperationTime}s</p>
-        <Stat percentage={(recipe.preperationTime/ 360 * 100)}/>
-        </a>
-    </div>
+        <div class="recipe aero-panel">
+            <button
+                on:click={() => (recipes = recipes.filter((x) => x != recipe))}
+                >-</button
+            >
+            <a draggable="false" href="recipes/{recipe.id}">
+                <h5>{recipe.name}</h5>
+                <h4>{recipe.description}</h4>
+                <p>Intensity: {recipe.intensity}</p>
+                <Stat percentage={(recipe.intensity / 10) * 100} />
+                <p>Prep time: {recipe.preperationTime}s</p>
+                <Stat percentage={(recipe.preperationTime / 360) * 100} />
+            </a>
+        </div>
     {/each}
 </div>
+
 <style>
-    h4{
+    h4 {
         margin-top: 0.2rem;
         margin-bottom: 0.2rem;
     }
-    .recipes{
+    .recipes {
         display: grid;
         gap: 1rem;
         grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
     }
-    .recipe button{
+    .recipe button {
         all: unset;
         font-size: 1.4rem;
         color: var(--color-text);
@@ -88,15 +49,15 @@ import type { Recipe } from "$lib/model/model";
         top: 0;
         right: 0;
     }
-    .recipe{
+    .recipe {
         transition: all 0.2s ease-in-out;
         position: relative;
         padding: 1rem;
     }
-    .recipe:hover{
+    .recipe:hover {
         background-color: var(--color-700);
     }
-    p{
+    p {
         margin-bottom: 0.4rem;
     }
 </style>
