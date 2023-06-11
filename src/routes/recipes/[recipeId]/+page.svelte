9d84f3c3d94edcc71e7058e3bdf629de2ed3f5c3
Samuel Middendorp <samiezaurus@gmail.com>
2023-06-11 18:51:25 +0200

    Fetch via fake api on recipe page

--- src/routes/recipes/[recipeId]/+page.svelte
+++ src/routes/recipes/[recipeId]/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+    import type { Recipe } from "$lib/model/model";
+    export let data;
+    let recipe: Recipe = data.recipe;
+</script>
 <div>
-    Recipe!
+    Recipe:
+    <h3>{recipe.name}</h3>
 </div>
+<style>
+    
+</style>
