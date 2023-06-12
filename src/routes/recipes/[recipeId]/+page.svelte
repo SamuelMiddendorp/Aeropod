ebc2a6b22a4e92d34b6d29d142b1d6ce02bf5e98
Samuel Middendorp <samiezaurus@gmail.com>
2023-06-12 18:37:33 +0200

    Add some steps to a recipe

--- src/routes/recipes/[recipeId]/+page.svelte
+++ src/routes/recipes/[recipeId]/+page.svelte
@@ -13,7 +13,17 @@
     <Stat percentage={(recipe.intensity / 10) * 100} />
     <p>Prep time: {recipe.preperationTime}s</p>
     <Stat percentage={(recipe.preperationTime / 360) * 100} />
+    <h3>Steps</h3>
     {#each recipe.steps as step}
-        <h3>{step.name}</h3>
+        <h4>{step.name}</h4>
+        <p>{step.description}</p>
     {/each}
 </div>
+<style>
+    h3{
+        margin-top: 1rem;
+    }
+    h4{
+        margin-top: 0.2rem;
+    }
+</style>
