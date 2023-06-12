89edb3914d04e9d0cd3ee0f316505a2871abb5a0
Samuel Middendorp <samiezaurus@gmail.com>
2023-06-12 18:34:58 +0200

    Show data on recipe page

--- src/routes/recipes/[recipeId]/+page.svelte
+++ src/routes/recipes/[recipeId]/+page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
+    import Stat from "$lib/components/Stat.svelte";
     import type { Recipe } from "$lib/model/model";
     export let data;
     let recipe: Recipe = data.recipe;
 </script>
+
 <div>
     Recipe:
     <h3>{recipe.name}</h3>
+    <p>{recipe.description}</p>
+    <p>Intensity: {recipe.intensity}</p>
+    <Stat percentage={(recipe.intensity / 10) * 100} />
+    <p>Prep time: {recipe.preperationTime}s</p>
+    <Stat percentage={(recipe.preperationTime / 360) * 100} />
+    {#each recipe.steps as step}
+        <h3>{step.name}</h3>
+    {/each}
 </div>
-<style>
-    
-</style>
