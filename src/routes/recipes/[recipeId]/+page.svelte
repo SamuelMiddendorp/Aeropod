bbf4d267c008ada1a5c46452b30ea16e48bb6a1c
Samuel Middendorp <samiezaurus@gmail.com>
2023-06-16 22:48:37 +0200

    Add description to the additional attributes

--- src/routes/recipes/[recipeId]/+page.svelte
+++ src/routes/recipes/[recipeId]/+page.svelte
@@ -8,13 +8,13 @@
 <div>
     <div class="properties">
         {#if recipe.coffeeIn}
-            <h3>{recipe.coffeeIn}</h3>
+            <h4>{recipe.coffeeIn} gram coffee</h4>
         {/if}
         {#if recipe.coffeeOut}
-            <h3>{recipe.coffeeOut}</h3>
+            <h4>{recipe.coffeeOut} ml water</h4>
         {/if}
         {#if recipe.inverted}
-            <h3>{recipe.inverted}</h3>
+            <h4>{recipe.inverted ? "Inverted" : "Normal"}</h4>
         {/if}
     </div>
     <h3>{recipe.name}</h3>
@@ -36,5 +36,17 @@
     }
     h4 {
         margin-top: 0.2rem;
+    }
+    .properties{
+        display: grid;
+        justify-items: center;
+        grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+    }
+    .properties h4{
+        border-radius: var(--border-radius);
+        display: grid;
+        place-content: center;
+        padding: 1rem;
+        background-color: var(--color-500);
     }
 </style>
