19ffe8b281e2c94eae9ed79454d064cbe39ce3c0
Samuel Middendorp <samiezaurus@gmail.com>
2023-06-23 22:05:13 +0200

    Set up gear grid

--- src/routes/gear/+page.svelte
+++ src/routes/gear/+page.svelte
@@ -6,9 +6,17 @@ let gear: Gear[] = getGear();
 
 </script> 
 <h3>Gear</h3>
+<div class="gear">
 {#each gear as item}
-<div class="gear item aero-panel">
+<div class="aero-panel">
     <h4>{item.name}</h4>
     <p>{item.category}</p>
-    </div>
+</div>
 {/each}
+</div>
+<style>
+    .gear{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
+    }
+</style>
