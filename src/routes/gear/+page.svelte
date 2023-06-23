1769930d473755d175956dc0d235f057726fa264
Samuel Middendorp <samiezaurus@gmail.com>
2023-06-23 21:48:14 +0200

    Fill gear page

--- src/routes/gear/+page.svelte
+++ src/routes/gear/+page.svelte
@@ -1,1 +1,14 @@
-Gear
+<script lang="ts">
+    import type { Gear } from "$lib/model/model";
+    import { getGear } from "$lib/stores/FakeAPIStore";
+
+let gear: Gear[] = getGear();
+
+</script> 
+<h3>Gear</h3>
+{#each gear as item}
+<div class="gear item aero-panel">
+    <h4>{item.name}</h4>
+    <p>{item.category}</p>
+    </div>
+{/each}
